630c2a3f36f73fb8d223493e6452732e15013d34
LAP1306\Chaam <21019021.thu@student.iuh.edu.vn>
2024-08-28 10:28:56 +0700

    myFilter - 28/08/2024

---
+++ ArrayFuntions/code/myReduce.js
@@ -0,0 +1,15 @@
+/// array
+var arr = [4, 6, 5, 1, 9, 11, 13];
+console.log(`arr: ${arr}`);
+
+//reduce
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+    var accumulator = initialValue === undefined ? undefined : initialValue;
+    for(let i = 0; i < this.length; i++) {
+        accumulator = accumulator != undefined ? callbackFn(accumulator, this[i]) : accumulator = this[0];
+    }
+    return accumulator;    
+}
+
+console.log(`arr reduce: ${arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}`);
+console.log(`arr myReduce: ${arr.myReduce((accumulator, currentValue) => accumulator + currentValue, 0)}`);
